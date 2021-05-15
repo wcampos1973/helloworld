0f458a4844937b46204ad7820a5cdb14c311c5ae
Willys Campos <willyscampos@RMB-McBkAir.local>
2021-05-15 09:55:27 -0300

    Primeiro Código

---
+++ script.js
@@ -0,0 +1,10 @@
+idade = 10;
+console.log('oi');
+console.log(idade);
+
+// action 
+function AcaoRepostaClickButton() {
+  let nomeInput = document.getElementById("NomeAluno").value;
+  //alert('fui clicado finalmente....');
+  alert('Muito bem! ' + nomeInput + " continue assim....");
+}
